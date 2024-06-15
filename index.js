41afdb8918eb64e2b30bd0384c384373ef9392ee
[it/its]@Rory& <[it/its]@Rory&>
2024-06-16 00:20:26 +0200

    Rewrite express server to be more dynamic

--- index.js
+++ index.js
@@ -1,83 +1,21 @@
-#!/bin/node
+#! /usr/bin/env node
+
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile("./webpage/index.html", {root: __dirname})
-})
-
-app.get('/login.html', (req, res) => {
-    res.sendFile("./webpage/login.html", {root: __dirname})
-})
-app.get('/register.html', (req, res) => {
-    res.sendFile("./webpage/register.html", {root: __dirname})
-})
-app.get('/register.js', (req, res) => {
-    res.sendFile("./webpage/register.js", {root: __dirname})
-})
-app.get('/dirrect.js', (req, res) => {
-    res.sendFile("./webpage/dirrect.js", {root: __dirname})
-})
-app.get('/contextmenu.js', (req, res) => {
-    res.sendFile("./webpage/contextmenu.js", {root: __dirname})
-})
-//
-app.get('/login.js', (req, res) => {
-    res.sendFile("./webpage/login.js", {root: __dirname})
-})
-app.get('/bitmap.svg', (req, res) => {
-    res.sendFile("./webpage/bitmap.svg", {root: __dirname})
-})
-app.get('/role.js', (req, res) => {
-    res.sendFile("./webpage/role.js", {root: __dirname})
-})
-app.get('/index.js', (req, res) => {
-    res.sendFile("./webpage/index.js", {root: __dirname})
-})
-app.get('/guild.js', (req, res) => {
-    res.sendFile("./webpage/guild.js", {root: __dirname})
-})
-app.get('/localuser.js', (req, res) => {
-    res.sendFile("./webpage/localuser.js", {root: __dirname})
-})
-app.get('/channel.js', (req, res) => {
-    res.sendFile("./webpage/channel.js", {root: __dirname})
-})
-app.get('/user.js', (req, res) => {
-    res.sendFile("./webpage/user.js", {root: __dirname})
-})
-app.get('/message.js', (req, res) => {
-    res.sendFile("./webpage/message.js", {root: __dirname})
-})
-app.get('/member.js', (req, res) => {
-    res.sendFile("./webpage/member.js", {root: __dirname})
-})
-app.get('/markdown.js', (req, res) => {
-    res.sendFile("./webpage/markdown.js", {root: __dirname})
-})
-app.get('/fullscreen.js', (req, res) => {
-    res.sendFile("./webpage/fullscreen.js", {root: __dirname})
-})
-app.get('/style.css', (req, res) => {
-    res.sendFile("./webpage/style.css", {root: __dirname})
-})
-app.get('/themes.css', (req, res) => {
-    res.sendFile("./webpage/themes.css", {root: __dirname})
-})
-app.get('/manifest.json', (req, res) => {
-    res.sendFile("./webpage/manifest.json", {root: __dirname})
-})
-
-app.get('/favicon.ico', (req, res) => {
-    res.send("")
-})
-
-app.get('/channels/*', async (req, res) => {
-    res.sendFile("./webpage/index.html", {root: __dirname})
+app.use('/', (req, res) => {
+    if(fs.existsSync(`./webpage${req.path}`)) {
+        res.sendFile(`./webpage${req.path}`, {root: __dirname});
+    }
+    else if(fs.existsSync(`./webpage${req.path}.html`)) {
+        res.sendFile(`./webpage${req.path}.html`, {root: __dirname});
+    }
+    else {
+        res.sendFile("./webpage/index.html", {root: __dirname});
+    }
 });
-
 
 const PORT = 8080;
 app.listen(PORT, () => {});
-console.log("this ran :P")
-
+console.log("this ran :P");
