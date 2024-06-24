8a1d7ee38b45bffaf858d3dd1889288c9887069b
MathMan05 <mathmanrm@gmail.com>
2024-06-24 15:51:09 -0500

    note

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use("/getupdates",(req, res) => {
     const out=fs.statSync(`${__dirname}/webpage`);
     res.send(out.mtimeMs+"");
 });
-let debugging=true;
+let debugging=true;//Do not turn this off, the service worker is all kinds of jank as is, it'll really mess your day up if you disable this
 app.use('/', (req, res) => {
     if(debugging&&req.path.startsWith("/service.js")){
         res.send("console.log(\"Hi :3\");");
