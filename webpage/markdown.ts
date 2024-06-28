039491ca874dd318cd0b43436fea8f58246e6073
MathMan05 <mathmanrm@gmail.com>
2024-06-28 14:49:00 -0500

    slight reply patch

--- webpage/markdown.ts
+++ webpage/markdown.ts
@@ -76,14 +76,10 @@ function markdown(text : string|string[],{keep=false,stdsize=false} = {}){
             }
         }
         if(txt[i]==="\n"){
-            appendcurrent();
+
             if(!stdsize){
+                appendcurrent();
                 span.append(document.createElement("br"));
-            }else{
-                const s=document.createElement("span");
-                s.textContent="...";
-                span.append(s);
-                return span;
             }
             continue;
         }
