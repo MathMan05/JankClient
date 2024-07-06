644e08e1654a38f6e1af738cb4cac9a0d85b1c68
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2024-07-06 09:38:37 +0200

    fix registration

--- webpage/register.js
+++ webpage/register.js
@@ -1,39 +1,52 @@
 const registertry = async event => {
 	event.preventDefault()
-	const elements = event.srcElement
-	const email = elements[1].value
-	const username = elements[2].value
-	if (elements[3].value != elements[4].value) {
+
+	if (document.getElementById("pass1").value != document.getElementById("pass2").value) {
 		document.getElementById("wrong").textContent = "Passwords don't match"
 		return
 	}
 
-	const dateofbirth = elements[5].value
-	const apiurl = new URL(JSON.parse(localStorage.getItem("instanceEndpoints")).api)
+	const email = document.getElementById("email").value
+	const apiUrl = new URL(JSON.parse(localStorage.getItem("instanceEndpoints")).api).toString()
 
-	fetch(apiurl + "/auth/register", {
+	const res = await fetch(apiUrl + "/auth/register", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify({
-			date_of_birth: dateofbirth,
+			date_of_birth: document.getElementById("birthdate").value,
 			email,
-			username,
-			password: elements[3].value,
-			consent: elements[6].checked
-		})
-	}).then(res => {
-		res.json().then(json => {
-			if (json.token) {
-				localStorage.setItem("token", json.token)
-				location.href = "/channels/@me"
-			} else {
-				console.log(json)
-				document.getElementById("wrong").textContent = json.errors[Object.keys(json.errors)[0]]._errors[0].message
-			}
+			username: document.getElementById("uname").value,
+			password: document.getElementById("pass1").value,
+			consent: document.getElementById("tos-check").checked
 		})
 	})
+
+	const json = await res.json()
+	if (json.token) {
+		localStorage.setItem("userinfos", JSON.stringify({
+			currentuser: email + apiUrl,
+			users: {
+				[email + apiUrl]: {
+					email,
+					pfpsrc: null,
+					serverurls: JSON.parse(localStorage.getItem("instanceEndpoints")),
+					username: document.getElementById("uname").value,
+					token: json.token
+				}
+			},
+			preferences: {
+				theme: "dark",
+				notifications: false,
+				notisound: "three"
+			}
+		}))
+		location.href = "/channels/@me"
+	} else {
+		console.log(json)
+		document.getElementById("wrong").textContent = json.errors[Object.keys(json.errors)[0]]._errors[0].message
+	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -44,12 +57,18 @@ document.addEventListener("DOMContentLoaded", () => {
 		const apiurl = new URL(JSON.parse(localStorage.getItem("instanceEndpoints")).api)
 		const tosPage = (await (await fetch(apiurl.toString() + "/ping")).json()).instance.tosPage
 		if (tosPage) {
-			document.getElementById("TOSbox").innerHTML = "I agree to the <a href=\"\" id=\"TOSa\" target=\"_blank\" rel=\"noopener\">Terms of Service</a>:"
+			document.getElementById("TOSbox").innerHTML = "I agree to the <a id=\"TOSa\" target=\"_blank\" rel=\"noopener\">Terms of Service</a>:"
 			TOSa = document.getElementById("TOSa")
 			TOSa.href = tosPage
+
+			document.getElementById("tos-check").disabled = false
+			document.getElementById("tos-check").checked = false
 		} else {
-			document.getElementById("TOSbox").textContent = "This instance has no Terms of Service, accept them anyways:"
+			document.getElementById("TOSbox").textContent = "This instance has no Terms of Service."
 			TOSa = null
+
+			document.getElementById("tos-check").disabled = true
+			document.getElementById("tos-check").checked = true
 		}
 		console.log("Found ToS page: " + tosPage)
 	}
