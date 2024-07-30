eb74509b7a7f1bbda16a481d4e0bfa54172e056f
MathMan05 <mathmanrm@gmail.com>
2024-07-30 15:45:26 -0500

    more error handling

--- .dist/snowflake.js
+++ .dist/snowflake.js
@@ -75,7 +75,13 @@ class SnowFlake {
         }
     }
     getUnixTime() {
-        return Number((BigInt(this.id) >> 22n) + 1420070400000n);
+        try {
+            return Number((BigInt(this.id) >> 22n) + 1420070400000n);
+        }
+        catch {
+            console.error(`The ID is corrupted, it's ${this.id} when it should be some number.`);
+            return 0;
+        }
     }
     toString() {
         return this.id;
