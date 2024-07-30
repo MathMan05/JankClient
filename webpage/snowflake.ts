b9f1bb5e53c33f20880cab2e05c32c51a015931e
MathMan05 <mathmanrm@gmail.com>
2024-07-30 15:45:26 -0500

    more error handling

--- webpage/snowflake.ts
+++ webpage/snowflake.ts
@@ -73,7 +73,12 @@ class SnowFlake<x extends WeakKey>{
         }
     }
     getUnixTime():number{
-        return Number((BigInt(this.id)>>22n)+1420070400000n);
+        try{
+            return Number((BigInt(this.id)>>22n)+1420070400000n);
+        }catch{
+            console.error(`The ID is corrupted, it's ${this.id} when it should be some number.`)
+            return 0;
+        }
     }
     toString(){
         return this.id;
