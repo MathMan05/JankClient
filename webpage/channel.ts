716566d9c468b6e12a8cca899edbc917212ba0f5
MathMan05 <mathmanrm@gmail.com>
2024-07-31 23:51:41 -0500

    more nice lookingness

--- webpage/channel.ts
+++ webpage/channel.ts
@@ -261,8 +261,9 @@ class Channel{
             const caps=document.createElement("div");
 
             const decdiv=document.createElement("div");
-            const decoration=document.createElement("b");
-            decoration.textContent="▼"
+            const decoration=document.createElement("img");
+            decoration.src="/icons/category.svg";
+            decoration.classList.add("svgtheme","colaspeicon")
             decdiv.appendChild(decoration)
 
             const myhtml=document.createElement("p2");
@@ -295,12 +296,12 @@ class Channel{
             childrendiv.classList.add("channels");
             setTimeout(_=>{childrendiv.style.height = childrendiv.scrollHeight + 'px';},100)
             decdiv.onclick=function(){
-                if(decoration.textContent==="▼"){//
-                    decoration.textContent="▲";
+                if(childrendiv.style.height!=='0px'){
+                    decoration.classList.add("hiddencat");
                     //childrendiv.classList.add("colapsediv");
                     childrendiv.style.height = '0px';
                 }else{
-                    decoration.textContent="▼";
+                    decoration.classList.remove("hiddencat");
                     //childrendiv.classList.remove("colapsediv")
                     childrendiv.style.height = childrendiv.scrollHeight + 'px';
                 }
