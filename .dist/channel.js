4a29f8a92c8fd6b048fe67a8666479cff9c75e99
MathMan05 <mathmanrm@gmail.com>
2024-09-03 11:28:08 -0500

    fix unread channels

--- .dist/channel.js
+++ .dist/channel.js
@@ -270,7 +270,10 @@ class Channel extends SnowFlake {
         if (!this.hasPermission("VIEW_CHANNEL")) {
             return false;
         }
-        return this.lastmessageid !== this.lastreadmessageid && this.type !== 4 && Boolean(this.lastmessageid);
+        return (!!this.lastmessageid) &&
+            ((!this.lastreadmessageid) ||
+                SnowFlake.stringToUnixTime(this.lastmessageid) > SnowFlake.stringToUnixTime(this.lastreadmessageid))
+            && this.type !== 4;
     }
     hasPermission(name, member = this.guild.member) {
         if (member.isAdmin()) {
--- webpage/channel.ts
+++ webpage/channel.ts
@@ -282,7 +282,12 @@ class Channel extends SnowFlake{
 		if(!this.hasPermission("VIEW_CHANNEL")){
 			return false;
 		}
-		return this.lastmessageid!==this.lastreadmessageid&&this.type!==4&&Boolean(this.lastmessageid);
+		return (!!this.lastmessageid)&&
+		(
+			(!this.lastreadmessageid)||
+			SnowFlake.stringToUnixTime(this.lastmessageid)>SnowFlake.stringToUnixTime(this.lastreadmessageid)
+		)
+		&&this.type!==4;
 	}
 	hasPermission(name:string,member=this.guild.member):boolean{
 		if(member.isAdmin()){
